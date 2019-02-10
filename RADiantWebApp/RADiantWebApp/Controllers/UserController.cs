f630fd7066b43cfdd92baae2f202d31f46b86ef3
Darris Cooper <d11198008@yahoo.com>
2019-02-09 19:37:00 -0800

    DC initial commit, added back end, mid tier

---
+++ RADiantWebApp/RADiantWebApp/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using RADiantWebApp.Models.Requests;
+using RADiantWebApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RADiantWebApp.Controllers
+{
+    public class UserController : ApiController
+    {
+        [RoutePrefix("api/User")]
+        public class WorkerController : ApiController
+        {
+            [HttpGet(), Route("echo")]
+            public IHttpActionResult EchoTest()
+            {
+                return Ok("Echo Success");
+            }
+
+            UserService svc = new UserService();
+
+            [HttpGet(), Route()]
+            public IHttpActionResult Get()
+            {
+                var status = svc.AllRecords();
+                if (status == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok(status);
+                }
+            }
+
+            [HttpGet(), Route("{id:int}")]
+            public IHttpActionResult Get(int id)
+            {
+                var status = svc.RecordById(id);
+                if (status == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok(status);
+                }
+            }
+
+            [HttpPost(), Route()]
+            public IHttpActionResult Post(UserAddRequest model)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    var status = svc.AddRecord(model);
+                    return Ok("User " + status + " has been added");
+                }
+            }
+
+            [HttpPut(), Route("{id:int}")]
+            public IHttpActionResult Put(UserEditRequest model)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    var status = svc.EditRecord(model);
+                    return Ok("User " + status + " had been updated");
+                }
+            }
+
+            [HttpDelete(), Route("{id:int}")]
+            public IHttpActionResult Delete(int id)
+            {
+                var status = svc.RemoveRecord(id);
+                if (status == 0)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok("User " + status + " has been removed");
+                }
+            }
+        }
+    }
+}
---
+++ RADiantWebApp/RADiantWebApp/Models/Domain/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RADiantWebApp.Models.Domain
+{
+    public class User
+    {
+        public int user_id { get; set; }
+
+        [Required(ErrorMessage = "Enter a value for first name")]
+        [StringLength(50)]
+        public string first_name { get; set; }
+
+        [Required(ErrorMessage = "Enter a value for last name")]
+        [StringLength(50)]
+        public string last_name { get; set; }
+
+        [Required(ErrorMessage = "Enter a value for email address")]
+        [StringLength(128)]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Enter a value for password")]
+        [StringLength(50)]
+        public string password { get; set; }
+
+        public int usage_id { get; set; }
+    }
+}
